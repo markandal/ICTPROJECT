ea3f3627236fefb3bd2ea962e78a5a096e8e163b
markzerra <andalghian14@gmail.com>
2024-08-27 10:32:57 +1000

    test

---
+++ services.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIRPLUS</title>
+</head>
+<body>
+
+  <div class="navbar">
+    <a href="contact.php">Contact</a>
+    <a href="about.php">About</a>
+    <a href="services.php">Services</a>
+    <a href="index.php">Home</a>
+  </div>
+
+
+  <h1>This is the Services page</h1>
+
+ 
